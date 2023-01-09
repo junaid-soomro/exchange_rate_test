af6df8f84799c4a778882406e71aad0e7bc61125
darklord <junaid-soomro@github.com>
2023-01-09 10:14:10 +0500

    added zappa settings and allowed all hosts

--- currency_exchange/settings.py
+++ currency_exchange/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-rh==&inx*)vmskxf^))kh2g590-u3awiog97iz)^k2&%rvpc$4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
